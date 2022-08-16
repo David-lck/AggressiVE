081114b9e4be2e3aaf7cb55c393d32359edc8f93
David-lck <chin.keat.lim@intel.com>
2022-08-16 10:17:19 +0800

    Updated Hang detection and System Reset Detection.
    - Hang Detection: Detect it's due to halt or really hang.
    - System Reset: Put it in all of 3 read-write so it can really detect the register will cause the reset without missing the chance to detect at the end of validation.

--- read_write.py
+++ read_write.py
@@ -522,6 +522,9 @@ class Val_stage:
             rd_in_list.append(two_read_value[1])
         else:
             rd_in_list.append(rd)
+        if target.readPostcode() != 0x10AD:#only for UEFI.
+            pass_fail = 'fail'
+            fail_reason.append('sys_rst')
         return wr_in_list,rd_in_list,pass_fail
 
     def second_stage_val(full_field_name,pre_rd,wr_in_list,rd_in_list,val_stage,wr_value):
@@ -550,11 +553,13 @@ def validate_1by1(full_field_name):#only on one chosen attr or all attrs.
             pass_fail = 'NA'
     else:
         pass_fail = 'pass'
-    if itp.isrunning() == False:
-        pass_fail = 'fail'
-        fail_reason.append('hang')
-    if target.readPostcode() != 0x10AD:
-        pass_fail = 'fail'
+    if itp.isrunning() == False:#If system has soft hang or cat error.
+        itp.go()
+        time.sleep(3)
+        if itp.isrunning() == False:
+            pass_fail = 'fail'
+            fail_reason.append('hang')
+    if 'sys_rst' in [pass_fail_pre_rd,pass_fail_1st_val,pass_fail_2nd_val,pass_fail_3rd_val]:
         fail_reason.append('sys_rst')
     return pre_rd,wr_in_list,rd_in_list,pass_fail,fail_reason
 
